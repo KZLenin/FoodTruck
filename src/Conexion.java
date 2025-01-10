1d3c06022311610b77ce84dbb356bdcd0f0cf107
KZLenin <leninmateo4@gmail.com>
2025-01-10 00:55:26 -0500

    Guardar informacion de la conexion a la database

--- src/Conexion.java
+++ src/Conexion.java
@@ -1,12 +1,30 @@
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Conexion {
-    private static final String URL = "jdbc:postgresql://pg-3451b877-bd-foodtruck.c.aivencloud.com:15972/db-restaurant?ssl=require&user=avnadmin&password=AVNS_4KzKkga0p_rnDDsOjK7";
+    private static String url;
+    private static String user;
+    private static String password;
+
+    static {
+        try (FileInputStream fis = new FileInputStream("config.properties")) {
+            Properties properties = new Properties();
+            properties.load(fis);
+            url = properties.getProperty("db.url");
+            user = properties.getProperty("db.user");
+            password = properties.getProperty("db.password");
+        } catch (IOException e) {
+            System.err.println("Error al cargar el archivo de configuración.");
+            e.printStackTrace();
+        }
+    }
 
     public void conectar() {
-        try (Connection connection = DriverManager.getConnection(URL)) {
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
             System.out.println("Conexión realizada exitosamente.");
         } catch (SQLException e) {
             System.out.println("Error al conectar a la base de datos.");
